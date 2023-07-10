866135a7587ecd189f2d87dd0497d63c588890d4
xth <2248617036@qq.com>
2023-07-10 16:46:51 +0800

    third commit

--- wms/src/main/java/com/wms/entity/GitTest.java
+++ wms/src/main/java/com/wms/entity/GitTest.java
@@ -12,5 +12,6 @@ package com.wms.entity;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Commit Test");
+        System.out.println("Git Commit Test22222");
     }
 }
