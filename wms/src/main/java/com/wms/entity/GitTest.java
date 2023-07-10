8d0607c056ec10f8e0650c822714fa4db26f6ce0
xth <2248617036@qq.com>
2023-07-10 16:44:58 +0800

    second commit

---
+++ wms/src/main/java/com/wms/entity/GitTest.java
@@ -0,0 +1,16 @@
+package com.wms.entity;
+
+/**
+ * ClassName: GitTest
+ * Package: com.wms.entity
+ * Description:
+ *
+ * @Author 谢天瀚
+ * @Create 2023/7/10 16:38
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Commit Test");
+    }
+}
